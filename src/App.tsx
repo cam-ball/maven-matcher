1f8229b069091c5a2f0a336ca5d792692f3725e4
cam-ball <hi@camball.me>
2024-12-21 19:40:17 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { questions } from './constants'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    hi
+    </>
+  )
+}
+
+export default App
